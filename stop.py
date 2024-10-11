e0b57431a1af31157bc629ee564146a3cc72458c
RaduLucianR <radlucradulescu@gmail.com>
2024-10-11 17:08:49 +0200

    cleanup

--- stop.py
+++
@@ -1,7 +0,0 @@
-from rpi_hardware_pwm import HardwarePWM
-import time
-
-pwm = HardwarePWM(pwm_channel=0, hz=16_000, chip=2)
-pwm.start(0) # full duty cycle
-
-pwm.stop()
--- tryagain.py
+++
@@ -1,8 +0,0 @@
-import time
-import os
-
-while True:
-	
-	val = os.system("pinctrl get 21")
-	print(val)
-	time.sleep(0.001)

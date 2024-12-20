66da2a4db842aaac81cc7697b58cdd84047ab781
RaduLucian <radlucradulescu@gmail.com>
2024-12-20 15:53:01 +0100

    stuff for ubuntu version

--- requirements.sh
+++ requirements.sh
@@ -5,3 +5,4 @@ sudo apt install python3-smbus
 pip install mpu6050 --break-system-package
 pip install evdev --break-system-package
 pip install rpi_hardware_pwm --break-system-package
+sudo apt install evtest
